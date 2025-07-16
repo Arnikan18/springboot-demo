4b96735c4ecdbb5305777ea8e374ac5ae9582c14
Arnikan18 <e21036@eng.pdn.ac.lk>
2025-07-16 18:55:17 +0530

    Saving student is added

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,11 +1,11 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
+//import java.time.LocalDate;
+//import java.time.Month;
 import java.util.List;
 
 @Service
@@ -22,6 +22,5 @@ public class StudentService {
 
     public List<Student> getStudents(){
         return studentRepository.findAll();
-        );
     }
 }
